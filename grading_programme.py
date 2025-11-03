7735c022ebe35749be1c96c728af707221517cbe
Waleed Saad Mohamed Alkalla <87426985+Abohamssa@users.noreply.github.com>
2025-11-04 00:28:00 +0200

    Create grading_programme.py

---
+++ grading_programme.py
@@ -0,0 +1,25 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+# Loop through the student_scores to get the score for each student
+
+for student in student_scores:
+    score = student_scores[student]
+    # convert score into grades based on the critera
+    if score >=91:
+        student_grades[student] = "Outstanding"
+    elif score >= 81:
+        student_grades[student] = "Exceeds Expectations"
+    elif score >= 71:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)
